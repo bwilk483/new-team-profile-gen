b2681d1013fca5e8309c429808abb42867bf2eb0
Bryan Wilkerson <bwilk483@gmail.com>
2022-08-19 22:34:20 -0500

    not working emp test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+test("employee test", () => {
+  const employee = new Employee("Bryan", 1, "bwilk483@gmail.com");
+  expect(enemy.name).toEqual("Bryan");
+  expect(enemy.id).toEqual(1);
+  expect(enemy.name).toEqual("bwilk483@gmail.com");
+  expect(enemy.getname()).toHaveProperty("name");
+  expect(enemy.getid()).toHaveProperty("id");
+  expect(enemy.get(email)).toHaveProperty("id");
+});
